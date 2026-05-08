6ca6c21e04e0f0d957a4d8c999d844f69d81d906
GuillenCanalesEloy <eloyguillencanales@gmail.com>
2026-05-08 12:57:55 -0500

    KAN-19: Inicialización de proyecto Spring Boot para Gestión Hotelera

---
+++ src/main/java/com/Grupo1/GestionHoteleria_Backend/GestionHoteleriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo1.GestionHoteleria_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHoteleriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHoteleriaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo1/GestionHoteleria_Backend/GestionHoteleriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo1.GestionHoteleria_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHoteleriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
